3bdba7d28c93dabd1c91ad39b866f530c87626d5
Pratik Karki <pratik@prertik.com>
2024-09-20 23:12:38 +0545

    Add validation and setter methods to various modules.
    Commented out code that doesn't compile, implemented validation and
    setter methods for the ticket structure, and completed size_of tests.
    Also added getters for title, description, and status, as well as a
    struct and validation for the Order module.
    
    Signed-off-by: Pratik Karki <pratik@prertik.com>

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,81 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16,
+}
+
+fn product_name_validator(product_name: String) -> String {
+    if product_name.is_empty() {
+        panic!("Product name can't be empty.")
+    } else if product_name.len() > 300 {
+        panic!("Product name can't be longer than 300 bytes.")
+    } else {
+        product_name
+    }
+}
+
+fn quantity_validator(quantity: u16) -> u16 {
+    if quantity <= 0 {
+        panic!("Quantity must be strictly greater than zero.")
+    } else {
+        quantity
+    }
+}
+
+fn unit_price_validator(unit_price: u16) -> u16 {
+    if unit_price <= 0 {
+        panic!("Quantity must be strictly greater than zero.")
+    } else {
+        unit_price
+    }
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u16, unit_price: u16) -> Order {
+        let validated_product_name = product_name_validator(product_name);
+        let validated_quantity = quantity_validator(quantity);
+        let validated_unit_price = unit_price_validator(unit_price);
+
+        Order {
+            product_name: validated_product_name,
+            quantity: validated_quantity,
+            unit_price: validated_unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u16 {
+        let total = &self.unit_price * &self.quantity;
+        total
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        let validated_product_name = product_name_validator(new_product_name);
+        self.product_name = validated_product_name
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u16) {
+        let validated_quantity = quantity_validator(new_quantity);
+        self.quantity = validated_quantity
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u16) {
+        let validated_unit_price = unit_price_validator(new_unit_price);
+        self.unit_price = validated_unit_price
+    }
+}
