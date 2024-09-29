837bfd28a9a135f0436be66fedc692ae1a45a3c4
Pratik Karki <pratik@prertik.com>
2024-09-29 16:49:23 +0545

    Refactor enums and structs, implement Drop and Add traits
    Refactor Ticket status to use an enum instead of a string, improving
    type safety. Implement the DropBomb struct with a Drop trait to ensure
    it panics if not defused before dropping. Add the SaturatingU16 struct
    and implement various conversion and addition traits for it, along with
    partial equality checks.
    
    Signed-off-by: Pratik Karki <pratik@prertik.com>

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,93 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Copy, Clone, Debug)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl SaturatingU16 {
+    pub fn new(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self { SaturatingU16::new(value) }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self { SaturatingU16::new(*value) }
+}
+
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self { SaturatingU16::new(value as u16) }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self { SaturatingU16::new(*value as u16) }
+}
+
+impl Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(other.value))
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: u16) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(other))
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &u16) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(*other))
+    }
+}
+
+impl Add<u8> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: u8) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(other as u16))
+    }
+}
+
+impl Add<&u8> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &u8) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(*other as u16))
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, other: &SaturatingU16) -> Self::Output {
+        SaturatingU16::new(self.value.saturating_add(other.value))
+    }
+}
+
+impl PartialEq for SaturatingU16 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
